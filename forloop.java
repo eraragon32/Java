98331388ecb58aedee6209c7b2ed43d3b9608941
eraragon32 <36524207+eraragon32@users.noreply.github.com>
2020-07-24 10:24:19 -0500

    7/24/2020

---
+++ forloop.java
@@ -0,0 +1,8 @@
+ class forloop {
+  public static void main(String[] args) {
+    String[] Subjects = {"English", "Maths", "Geo", "Science"};
+    for (String i : Subjects) {
+      System.out.println(i);
+    }    
+  }
+}
---
+++ multi2.java
@@ -0,0 +1,8 @@
+ class multi2 {
+ public static void main(String[] args) {
+		int i=2;
+ for (int j=1;j<=10;j++) {
+      System.out.printf("%d*%d=%d \n",i,j,i*j);
+    }    
+  }
+}
